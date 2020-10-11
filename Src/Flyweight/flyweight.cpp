0aa1ee8863aec740733f04538e01e10eab82d476
Barret Ren <barret.ren@outlook.com>
2020-10-11 11:46:27 +0800

    update flyweight code

--- Src/Flyweight/flyweight.cpp
+++ Src/Flyweight/flyweight.cpp
@@ -1,10 +1,8 @@
-
 class Font
 {
 private:
-    //unique object key
+    //unique object key，用于查找指定对象
     string key;
-
     //object state
     //....
 
@@ -18,20 +16,19 @@ public:
 class FontFactory
 {
 private:
-    map<string, Font *> fontPool;
+    map<string, Font *> fontPool; //对象池
 
 public:
     Font *GetFont(const string &key)
     {
-
         map<string, Font *>::iterator item = fontPool.find(key);
 
         if (item != footPool.end())
         {
-            return fontPool[key];
+            return fontPool[key];//对象池存在，直接返回
         }
         else
-        {
+        {//找不到对象，创建新对象返回
             Font *font = new Font(key);
             fontPool[key] = font;
             return font;
